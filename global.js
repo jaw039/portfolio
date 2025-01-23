8e177c6a2be029f1542518f2482c37aab251dba7
Jackie Wang <jaw039@ucsd.edu>
2025-01-22 20:03:30 -0800

    checking for routing

--- global.js
+++ global.js
@@ -18,44 +18,45 @@
 // currentLink.classList.add('current');
 
 let pages = [
-    { url: '', title: 'Home' },
-    { url: '/projects/', title: 'Projects' },
-    { url: '/contact/', title: 'Contact' },
-    { url: '/resume/', title: 'Resume' },
+    { url: './', title: 'Home' },
+    { url: './projects/', title: 'Projects' },
+    { url: './contact/', title: 'Contact' },
+    { url: './resume/', title: 'Resume' },
     { url: 'https://github.com/jaw039', title: 'Profile' }
   ];
   
-  // Add <nav> to the page
-  let nav = document.createElement('nav');
-  document.body.prepend(nav);
-  
-  // Detect if we are on the home page
-  const ARE_WE_HOME = document.documentElement.classList.contains('home');
-  
-  // Add links to the navigation menu
-  for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-  
-    // Adjust URL for non-home pages
-    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-  
-    // Create the <a> element
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
 
-    // Add the "current" class to the current page link
-    a.classList.toggle('current', location.host && a.pathname === location.pathname);
-  
-  // Add target = "_blank" to external links
-  // Add target = "_blank" to external links
-  if (a.host !== location.host) {
-    a.target = '_blank';
-  }
+// Add <nav> to the page
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-  // Append the link to the navigation menu
-  nav.append(a);
+// Detect if we are on the home page
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+// Add links to the navigation menu
+for (let p of pages) {
+let url = p.url;
+let title = p.title;
+
+// Adjust URL for non-home pages
+url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+// Create the <a> element
+let a = document.createElement('a');
+a.href = url;
+a.textContent = title;
+
+// Add the "current" class to the current page link
+a.classList.toggle('current', location.host && a.pathname === location.pathname);
+
+// Add target = "_blank" to external links
+// Add target = "_blank" to external links
+if (a.host !== location.host) {
+a.target = '_blank';
+}
+
+// Append the link to the navigation menu
+nav.append(a);
 }
 
 // Add dark mode switch to top right corner
